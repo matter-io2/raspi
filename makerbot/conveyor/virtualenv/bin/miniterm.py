f9e32881c441b9eba65739a98cc19e3c9b81fa7e
gtaomatter <greg@matter.io>
2013-07-07 16:39:38 -0400

    added virtualenv/bins

---
+++ makerbot/conveyor/virtualenv/bin/miniterm.py
@@ -0,0 +1,5 @@
+#!/home/pi/makerbot/conveyor/virtualenv/bin/python
+# EASY-INSTALL-SCRIPT: 'pyserial==2.7-mb2.1','miniterm.py'
+__requires__ = 'pyserial==2.7-mb2.1'
+import pkg_resources
+pkg_resources.run_script('pyserial==2.7-mb2.1', 'miniterm.py')
---
+++ makerbot/conveyor/virtualenv/bin/unit2.py
@@ -0,0 +1,5 @@
+#!/home/pi/makerbot/conveyor/virtualenv/bin/python
+# EASY-INSTALL-SCRIPT: 'unittest2==0.5.1','unit2.py'
+__requires__ = 'unittest2==0.5.1'
+import pkg_resources
+pkg_resources.run_script('unittest2==0.5.1', 'unit2.py')
