e73eccae8deffe6471bf14431bb526c5550c13de
gtaomatter <greg@matter.io>
2013-07-09 17:03:30 -0400

    added webcam routine - 1st attempt

---
+++ pinger/webcam_routine.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# filename_local = '/home/pi/Desktop/printer_pic.jpg'
+# fswebcam -r 940x720 -S 8 -d /dev/video0 --jpeg 95 --save $filename_local
+# curl -F "file=@$filename_local;filename=$0.jpg" 
+
+fswebcam -r 940x720 -S 8 -d /dev/video0 --jpeg 95 --save /home/pi/Desktop/printer_pic.jpg
+curl -F "file=@/home/pi/Desktop/printer_pic.jpg;filename=$0.jpg" 
+
+
+
+
+
+#curl reference
+#http://curl.haxx.se/docs/manpage.html#-F
+#-F "file=@/filename/;filename="
+#--data ensures the info is uploaded as post
+#type=image/jpg
+#--request POST
