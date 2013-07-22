f88a1a75cebb66dcaf8d8826e4cdfcf857fcac21
Greg Tao <greg@matter.io>
2013-07-22 18:22:27 -0400

    working curl commands yay!

--- pinger/log_upload.sh
+++ pinger/log_upload.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
-# filename_local = '/home/pi/Desktop/printer_pic.jpg'
-# curl -F "file=@$filename_local;filename=$0.jpg" 
+#DEBUG input
+#echo server_address = $1
+#echo logPath = $2
+#echo job_id passed = $3,
 
-echo server_address = $1
-echo logPath = $2
-echo job_id passed = $3,
-curl -F "file=@$2;filename=$3.log" -m 15 $1
+#defines return format
+output=",%{url_effective},%{size_upload},%{speed_upload},%{time_total},%{time_connect}"
+
+#upload call
+curl -F "file=@$2;filename=$3.log" -m 15 -s -w $output $1
 #sudo rm /home/pi/raspi/pinger/pinger.log
+
 
 #curl reference
 #http://curl.haxx.se/docs/manpage.html#-F
--- pinger/webcam_routine.sh
+++ pinger/webcam_routine.sh
@@ -5,13 +5,13 @@
 # curl -F "file=@$filename_local;filename=$0.jpg" 
 
 vid=$(find /dev -name 'video0' | wc -l)
-echo vid = $vid
 if [ "$vid" -eq "1" ]
 then 
-	echo server_address = $1
-	echo printerId passed = $2,
-	echo pic_count = $3
-	echo debug_webcam = $4
+	#echo server_address = $1
+	#echo printerId passed = $2,
+	#echo pic_count = $3
+	#echo debug_webcam = $4
+	
 	pic_num=$3
 	old_pic=$((pic_num-3))
 	#fswebcam -r 960x544 -S 8 -d /dev/video0 --jpeg 95 --save /home/pi/Des
@@ -20,26 +20,20 @@ then
 		#clear everything when pinger starts
 		sudo rm /dev/shm/*.jpg
 	else
-		echo 'clearing /dev/shm'
+		# echo 'clearing /dev/shm'
 		sudo rm /dev/shm/$2_$old_pic.jpg # clear ram to prevent taking too much space
-		echo 'ram cleared'
+		# echo 'ram cleared'
 	fi
 
-	#fswebcam -r 640x480 -S 8 -d /dev/video0 --jpeg 95 --save /dev/shm/$2.jpg
 	fswebcam -c fswebcam.conf --save /dev/shm/$2_$3.jpg  #loads options that are in /raspi/pinger folder
 
-	echo "starting http post"
-	#matter server
-	#curl -F "file=@/dev/shm/$2_$3.jpg;filename=$2_$3.jpg" -m 15 http://matter.io/webcamUpload
-	#curl -F "file=@/dev/shm/$2_$3.jpg;filename=$2_$3.jpg" -m 15 http://ec2-107-22-186-175.compute-1.amazonaws.com/webcamUpload
-
-	curl -F "file=@/dev/shm/$2_$3.jpg;filename=$2_$3.jpg" -m 15 $1
-
-	echo "post finished"
-
+	#defines return format
+	output=",%{url_effective},%{size_upload},%{speed_upload},%{time_total},%{time_connect}"
+	curl -F "file=@/dev/shm/$2_$3.jpg;filename=$2_$3.jpg" -m 15 -s -w $output $1
 else 
-	echo no webcam attached
+	echo 'no webcam attached'
 fi
+echo vid:$vid,
 
 #curl reference
 #http://curl.haxx.se/docs/manpage.html#-F
@@ -47,3 +41,6 @@ fi
 #--data ensures the info is uploaded as post
 #type=image/jpg
 #--request POST
+
+#example fswebcam call from commandline
+#fswebcam -r 640x480 -S 8 -d /dev/video0 --jpeg 95 --save /dev/shm/$2.jpg
